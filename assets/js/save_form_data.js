748259c8493f943f5e74944cedbb358aff05261a
Sai Khant Zay Lynn Yaung <khantzay.ly@gmail.com>
2023-09-27 13:43:39 +0630

    update

--- assets/js/save_form_data.js
+++ assets/js/save_form_data.js
@@ -4,15 +4,15 @@ contactForm.addEventListener('submit', function (e) {
   e.preventDefault(); // Prevent the default form submission
 
   // Get the contact form data
-  const user_name = contactForm.querySelector('input[name="user_name"]').value;
-  const user_email = contactForm.querySelector('input[name="user_email"]').value;
-  const message = contactForm.querySelector('textarea[name="message"]').value;
+  const user_name = contactForm.querySelector('input[name="user_name"]');
+  const user_email = contactForm.querySelector('input[name="user_email"]');
+  const message = contactForm.querySelector('textarea[name="message"]');
 
   // Create a JavaScript object with the form data
   const formData = {
-    user_name: user_name,
-    user_email: user_email,
-    message: message
+    user_name: user_name.value,
+    user_email: user_email.value,
+    message: message.value
   };
 
   // Convert the JavaScript object to JSON
@@ -32,6 +32,11 @@ contactForm.addEventListener('submit', function (e) {
       if (data.status === 'success') {
         // Show a success message to the user
         alert('Message sent successfully!');
+        
+        // Clear the form fields
+        user_name.value = '';
+        user_email.value = '';
+        message.value = '';
       } else {
         // Show an error message to the user
         alert('Error sending message. Please try again later.');
